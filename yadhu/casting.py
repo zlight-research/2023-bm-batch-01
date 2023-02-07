d0721015cbc4840c7aae12a1d1848dd385aeba8f
yadhu1077 <112114346+yadhu1077@users.noreply.github.com>
2023-02-07 16:52:06 +0530

    My First Program In Zligth

---
+++ yadhu/casting.py
@@ -0,0 +1,8 @@
+#1)How to display the current date format of "2022, September 12 11:00 AM" ?
+import datetime
+"""
+current_date is a varible used to store the current date and time
+strftime is use to convert dates to strings
+"""
+current_date=datetime.datetime.now()
+print(current_date.strftime("Current Date And Time=%Y,%b %d  %H:%M:%S %p"))
---
+++ yadhu/datatypes.py
@@ -0,0 +1,19 @@
+#2)Receive input from various data types and display it?
+"""
+type()function returns the type of the object 
+"""
+integer=10
+print("type of data",type(integer),"data=",integer)
+float=7.7
+print("type of data=",type(float),"data=",float)
+string="yadhu"
+print("type of data=",type(string),"data=",string)
+boolean=True
+print("type of data=",type(boolean),"data=",boolean)
+import datetime
+'''
+here we import a datetime module
+now() is used to get the current date and time
+'''
+data=y=datetime.datetime.now() 
+print("type of data=",type(data),"data=",data)
