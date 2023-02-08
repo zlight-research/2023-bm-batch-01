3f5b58ac10acc2816e90db801f21bd15e6ff7eec
vishakhh <vishakhks22@gmail.com>
2023-02-08 12:49:27 +0530

    today work

---
+++ vishakh/day-01/data_types.py
@@ -0,0 +1,33 @@
+#2)Receive input from various data types and display it?
+
+#everything is an object in python programming data types are actually classes and variables are instance(objects)of these classes
+#data types are the classification or catagorization of data items
+
+#some data and its data types
+
+data1=23
+print(type(data1),data1)#'integer type'
+print("*"*100)
+
+
+data2=2.4
+print(type(data2),data2)#'float type'
+print("*"*100)
+
+data3=True
+print(type(data3),data3)#'boolean type'
+print("*"*100)
+
+
+data4=5+4j
+print(type(data4),data4)#'complex type'
+print("*"*100)
+
+
+data5="vishakh"
+print(type(data5),data5)#'string type'
+print("*"*100)
+
+
+
+
