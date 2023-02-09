beeb497c63c2acd808ca28449719b8534866df84
Aparna <107956792+aparnamathew95@users.noreply.github.com>
2023-02-09 09:22:17 +0530

    Add files via upload

---
+++ day1/truefalse.py
@@ -0,0 +1,12 @@
+# Accept boolean variable and display "F" for false,"M" for true
+
+var = input("enter the boolean value:")
+age = var.upper()
+
+if age == "FALSE":
+    print('F')
+    
+# elif evaluated to true.
+elif age == "TRUE":
+    print('M')
+
