2cd53f121b2f28bb3c1cc86554529a09b36123d5
aparnamathew95 <aparnaambattu@gmail.com>
2023-02-10 22:50:36 +0530

    programs

---
+++ Aparna/day2/split.py
@@ -0,0 +1,5 @@
+# 1)How to split the names in based on special character 
+name="zligth research"  # Variable declaration
+split=name.split() #Calling function
+print("first_word=",split[0].capitalize()) #print result 
+print("second_word=",split[1].capitalize()) #print result 
