7d8a5fee5224d32b7dbc61f0c44bc4111385ecd8
Angithasheeban <angiacha535@gmail.com>
2023-02-11 19:08:27 +0530

    chngee

--- Angitha/day1/casting.py
+++ Angitha/day1/casting.py
@@ -1,12 +1,25 @@
 num=10
-# this is integer value
+"""this is integer value"""
 num=float(num)
 # int convert to float
 print(type(num),num)
 
 
 num=10.12
-# this is float value
+"""this is float value"""
 num=int(num)
-# int convert to int
+"""int convert to int"""
+print(type(num),num)
+
+
+num="52"
+"""this is string value"""
+num=int(num)
+"""int convert to int"""
+print(type(num),num)
+
+num=52
+"""this is string value"""
+num=str(num)
+"""int convert to int"""
 print(type(num),num)
