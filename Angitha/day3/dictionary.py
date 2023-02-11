2d50c1555325fb61e70538e3b61ad397b705afa8
Angithasheeban <angiacha535@gmail.com>
2023-02-11 09:58:08 +0530

    dict

--- Angitha/day3/dictionary.py
+++ Angitha/day3/dictionary.py
@@ -1,27 +1,29 @@
-#  Please sort the dictionary based on Age
+"""Please sort the dictionary based on Age"""
 
-school={'Ajay':16,'syam':22,'yadhu':25}
-print(school)
-
-# Change the Name of id = 3 from syam to Shobhana
-school={'Ajay':16,'syam':22,'yadhu':25}
-school['shobana']=school['yadhu']
-del school['yadhu']
-print(school)
-
-# Update Gender "Male" only for "Ajay" record.
-school={'Ajay':16,'syam':22,'yadhu':25}
-school['Gender']='male'
-print(school)
+school=[{"id":1, "name":"Yadhu", "age":25},
+{"id":2, "name":"Ajay", "age":16},
+{"id":3, "name":"Syam", "age":22}]
+print("sorting by age:")
+"""firstly we sort only the age in ascendeing order"""
+print(sorted(school,key=lambda i:i['age']))
+"""then we sort """
+"""lambda we use supplying a fn as a key parameter to sorted"""
 
 
-school=[
-{"id":1, "name":"Yadhu", "age":25},
+
+
+"""Change the Name of id = 3 from syam to Shobhana"""
+
+school=[{"id":1, "name":"Yadhu", "age":25},
 {"id":2, "name":"Ajay", "age":16},
 {"id":3, "name":"Syam", "age":22}]
 
-
+school[0]["name"]="shobana"
+"""In this we can rename the yadhu to shobana"""
+"""[0] is the index of list"""
 print(school)
+
+
 
 
 
