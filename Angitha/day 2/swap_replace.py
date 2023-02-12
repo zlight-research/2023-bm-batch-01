61da4a05ee12e568eec2b7be6f9827f7e15c913b
Angithasheeban <angiacha535@gmail.com>
2023-02-12 21:37:51 +0530

    day2

---
+++ Angitha/day 2/swap_replace.py
@@ -0,0 +1,12 @@
+"""Text = “created my first program in research zlight”
+Replace the text “first” to “second”
+Swap the words research and zlight
+Displayed as”Created my second program in Zlight Research Pvt Ltd” """
+
+
+"""Replace the text “first” to “second” """
+company =" created my first program in research zlight"
+wrk=company.replace("first","second")
+print(wrk)
+
+
