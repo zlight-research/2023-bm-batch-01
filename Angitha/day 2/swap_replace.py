4b038e39e08572897b0126ebafac87144c80a3d7
Angithasheeban <angiacha535@gmail.com>
2023-02-12 21:44:38 +0530

    chnge

--- Angitha/day 2/swap_replace.py
+++ Angitha/day 2/swap_replace.py
@@ -7,6 +7,7 @@ Displayed as”Created my second program in Zlight Research Pvt Ltd” """
 """Replace the text “first” to “second” """
 company =" created my first program in research zlight"
 wrk=company.replace("first","second")
+"""In this replace to use new wrd can display"""
 print(wrk)
 
 
