7cbef994e37cd2c718a73dce61eb864a5d369bde
Angithasheeban <angiacha535@gmail.com>
2023-02-12 22:08:01 +0530

    chngee

--- Angitha/day 2/swap_replace.py
+++ Angitha/day 2/swap_replace.py
@@ -11,3 +11,13 @@ wrk=company.replace("first","second")
 print(wrk)
 
 
+"""Swap the words research and zlight"""
+company="created my first program in research zlight"
+words=company.split()
+"""split sentence into words"""
+words[5],words[6]=words[6],words[5]
+"""swap research and zlight"""
+new_sentence=" ".join(words)
+"""convert into strings"""
+print(new_sentence)
+
