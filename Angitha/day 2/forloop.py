e7a9009db8489b26e8f85c37d3f4fd1244d5b64d
Angithasheeban <angiacha535@gmail.com>
2023-02-14 08:22:43 +0530

    changes

--- Angitha/day 2/forloop.py
+++ Angitha/day 2/forloop.py
@@ -1,15 +1,32 @@
 """Use FOR and WHILE loop to display the below manner."""
 """EVEN NUMBER"""
-for num in range(10,0,-2): 
-    """Here we give for loop and give Range(10,0,-2,) 
-    Here 10 to 0 numbers """
+# for num in range(10,0,-2): 
+#     """Here we give for loop and give Range(10,0,-2,) 
+#     Here 10 to 0 numbers """
       
-    print(num)
+#     print(num)
 
-"""ODD NUMBER"""
-for num in range(9,0,-2): 
-    """Here we give for loop and give Range(9,0,-2,) 
-    Here 9 to 0 numbers """
+# """ODD NUMBER"""
+# for num in range(9,0,-2): 
+#     """Here we give for loop and give Range(9,0,-2,) 
+#     Here 9 to 0 numbers """
       
-    print(num)   
+#     print(num)  
+# num=int(input("enter the number:"))
+# for num in range(10,0,-2): 
+#     for number in range(9,0,-2):
+#         if(number % 2 != 0):
+#             print("{0}".format(number))
+ 
 
+# print("Element in Even List is : ", Even)
+# print("Element in Odd List is : ", Odd)
+
+print("Even num:")
+for i in range(10,0,-2):
+    if(i%2==0):
+        print(i)
+print(" Odd num:")
+for i in range(9,0,-2):
+    if(i%2!=0):
+        print(i)
