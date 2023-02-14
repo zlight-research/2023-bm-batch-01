cba03dc4d26095355ce2591a4d588a6a34f63ddf
sreelekshmi123 <sreelekshmisree563@gmail.com>
2023-02-14 22:36:56 +0530

    day-02

---
+++ sreelekshmi s/day-01/day-02/display.py
@@ -0,0 +1,14 @@
+"""Text = “created my first program in research zlight”
+
+Replace the text “first” to “second”
+Swap the words research and zlight
+Displayed as”Created my second program in Zlight Research Pvt Ltd”. """
+
+
+
+Text1="created my first program in research zlight"
+Text2=" Pvt Ltd "
+Text=Text1 + Text2       #concatenation of two string
+x=Text.replace("first" , "second")     #replace the words 
+print(x)
+
---
+++ sreelekshmi s/day-01/day-02/forwhile.py
@@ -0,0 +1,34 @@
+#Use FOR and WHILE loop to display the below manner.
+#Even Numbers : 10, 8, 6,4,2
+#Odd Numbers : 9,7,6,5,3,1
+ 
+
+#WHILE LOOP
+
+print("odd  even")
+i=1
+while i<10:
+    print(f"{i}\t{i+1}")
+    i+=2
+
+
+
+
+#FOR LOOP
+
+i=1
+end=10
+for i in range(i, end):
+    if  i%2==0:
+      print("even number:",i,end=" " )
+    else:
+      print("Odd number:",i,end=" " )
+
+
+      
+
+
+
+
+
+
