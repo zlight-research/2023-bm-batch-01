f2098f3a119d9bbbed9875215da2c63831e6f43c
sreelekshmi123 <sreelekshmisree563@gmail.com>
2023-02-15 17:21:25 +0530

    day--01

--- sreelekshmi s/day--01/truefalse.py
+++ sreelekshmi s/day--01/truefalse.py
@@ -1,7 +1,22 @@
 # Accept boolean variable and display "F" for False, "M" for True
-a=14
-b=15
-if b > a:
-  print("M")
-else:
-  print("F")
+"""if the user input is true then print "M", if it is false then print "F" , 
+then its convert true to "M"  and false to "F" """
+
+
+colour=input("red and black is colour, true or false?:")
+if colour==("true"):
+    print("M")
+elif colour==("false"):
+    print("F")
+
+    
+
+
+
+
+
+
+
+
+
+
