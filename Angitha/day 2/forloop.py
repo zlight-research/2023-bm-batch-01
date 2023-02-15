3c359c0f9f38cc97fb0aaa228cce5cfba0c522f5
Angithasheeban <angiacha535@gmail.com>
2023-02-15 09:08:42 +0530

    chnge

--- Angitha/day 2/forloop.py
+++ Angitha/day 2/forloop.py
@@ -1,11 +1,39 @@
 """Use FOR and WHILE loop to display the below manner."""
-"""EVEN NUMBER"""
-print("Even num:")
-for i in range(10,0,-2):
-    if(i%2==0):"""In even number we get i%2===0"""
-    print(i)
-"""ODD NUMBER"""
-print(" Odd num:")
-for i in range(9,0,-2):
-    if(i%2!=0):"""In odd number we get i%2===0"""
-    print(i)
+# n=[] 
+# for n in range(10):
+#     if (n%2==0 and n%2!=0):
+#         print("Even number:")
+#         n.()
+#     else:
+#         print("Odd number:")
+# print(n)
+
+# obj = ["Even" if i%2==0 else "odd" for i  in range(20)] 
+# print(obj)
+
+
+num_list = [10,9,8,7,6,5,4,3,2,1]
+even =[]
+odd = []
+for num in num_list:
+    if (num % 2 == 0 and num%2!=0):
+        even.append(num)
+    else:
+        odd.append(num)
+print(num_list)
+print("Even Number:") 
+print("Odds Number:") 
+
+ 
+
+    
+
+		
+		 
+			
+		
+			
+ 
+    
+
+
