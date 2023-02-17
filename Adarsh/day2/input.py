ddea1312b9db8b3ea82d2fa41fead259a7ec839b
adarshanil2000 <17adarshanil@gmail.com>
2023-02-17 10:00:14 +0530

    committ 2

--- Adarsh/day2/input.py
+++ Adarsh/day2/input.py
@@ -1,25 +1,42 @@
-# 4)Enter input from user?
-first_name=input("enter first name:")
-last_name=input("enter last name:")
-gender=input("enter gender:")
-age=int(input("enter age:"))
-rating=int(input("enter rating:"))
-gender_up=gender.upper()
-# to check the conditions
-if gender_up=="MALE":
-    print("name:Mr",last_name.capitalize(),first_name.capitalize())
-elif gender_up=="FEMALE":
-    if age<=30:
-       print("name:Miss",last_name.capitalize(),first_name.capitalize())
+""" 4). Enter input using python code"""
+
+
+d = { } #declaring empty dictionary
+for i in range(1):
+    name = input("First Name: ") 
+    lastname = input("Last Name: ") 
+    Gender = input("Gender: ")
+    Age=int(input("Age: "))
+    Rating=int(input("Rating: "))
+
+#------assinging the keys with the values---------
+
+    d["name"] = name.capitalize() 
+    d["lastname"] = lastname .capitalize()
+    d["gender"] = Gender.capitalize()
+    d["age"] = Age
+    d["rating"] = Rating
+
+     
+#--------defining the salutation using gender and age in if condition------
+
+if d["gender"]=="MALE":
+    print("Name: Mr.",d["lastname"],d["name"])
+elif d["gender"]=="FEMALE":
+    if d["age"]<=30:
+       print("name: Miss.",d["lastname"],d["name"])
     else:
-       print("name:Mrs",last_name.capitalize(),first_name.capitalize())
+       print("name: Mrs.",d["lastname"],d["name"])
+
+#---------display the rating table using rating in if condition-------
+
 print("Rating Table")
-print("********")
-if rating>100:
-    print("Performance Result : Extra Milestone")
-elif rating>=90 and rating<=100:
-    print("Performance Result : Outstanding")
-elif rating>=60 and rating<=89:
-    print("Performance Result : Good")
+print(d["rating"])
+if d["rating"]>100:
+    print("Rating Result : Extra Milestone")
+elif d["rating"]>=90 and d["rating"]<=100:
+    print("Rating Result : Outstanding")
+elif d["rating"]>=60 and d["rating"]<=89:
+    print("Rating Result : Good")
 else:
-    print("Performance Result : Needs Improvement")
+    print("Rating Result : Needs Improvement")
