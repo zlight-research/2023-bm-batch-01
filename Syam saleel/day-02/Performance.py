baa4bc637fe43df07912ced12f80c9ba2256aa82
Syamsaleel <Syamsaleel4066@gmail.com>
2023-02-20 09:28:02 +0530

    change

---
+++ Syam saleel/day-02/Performance.py
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+# Input values
+first_name = input("Enter First Name: ").capitalize()
+last_name = input("Enter Last Name: ")
+gender = input("Enter Gender (M/F): ")
+age = int(input("Enter Age: "))
+rating = int(input("Enter Rating: "))
+
+# Get salutation based on gender
+if gender == "M":
+    salutation = "Mr."
+elif gender == "F":
+    salutation = "Mrs/miss" 
+else:
+    salutation = ""
+
+# Construct full name
+if first_name and last_name:
+    full_name = f"{salutation} {first_name},{last_name} "
+else:
+    full_name = first_name + last_name
+
+# Determine performance result based on rating
+if rating > 100:
+    performance = "Extra Milestone"
+elif rating >= 90:
+    performance = "Outstanding"
+elif rating >= 60:
+    performance = "Good"
+else:
+    performance = "Needs Improvement"
+
+# Display results
+print(f"Name: {full_name}")
+
+print(f"Performance Result : {performance}")
+
+
+
+
