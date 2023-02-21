723b8f29b79c0f31e85d79dd28513dd9d0a0fc75
sreelekshmi123 <sreelekshmisree563@gmail.com>
2023-02-21 21:24:46 +0530

    chnge

--- sreelekshmi s/day-03/complexdata2nd.py
+++ sreelekshmi s/day-03/complexdata2nd.py
@@ -16,9 +16,10 @@ original_list = [{"id":1, "f_name":"yadhu", "l_name":"Kumar", "gender":"M", "age
                 {"id":3, "f_name":"syam", "l_name":"Warrier", "gender":"F", "age":22}]
 
 new_dict = {"Age <= 20": []}   #new dict contain age below 20
-new_dict1 = { "Age > 20": []} #new dict contain age above  20
+new_dict1 = { "Age > 20": []}  #new dict contain age above 20
 for item in original_list:
     full_data = {"f_name": item["f_name"], "l_name": item["l_name"],"id":item['id']}
+
     if item["age"] <= 20:
         new_dict["Age <= 20"].append(full_data)
     else:
---
+++ sreelekshmi s/day-04/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+import os
+old_name="C:\\zlight\\2023-bm-batch-01-1\\sreelekshmi s\\day-04\\day-04.txt"
+new_name="C:\\zlight\\2023-bm-batch-01-1\\sreelekshmi s\\day-04\\\\sting.txt"
+os.rename(old_name,new_name)
+os.chdir(path='C:\Task')
