5789feae0cd0e140cd2bf3d6fb829f8c2d99857c
Angithasheeban <angiacha535@gmail.com>
2023-02-21 23:12:19 +0530

    chnge

--- Angitha/day3/dictionary.py
+++ Angitha/day3/dictionary.py
@@ -19,8 +19,8 @@ school=[{"id":1, "name":"Yadhu", "age":25},
 {"id":3, "name":"Syam", "age":22}]
 
 school[2]["name"]="shobana"
-"""In this we can rename the yadhu to shobana"""
-"""[0] is the index of list"""
+"""Rename """
+
 print(school)
 
 """Update Gender "Male" only for "Ajay" record"""
@@ -28,7 +28,7 @@ school=[{"id":1, "name":"Yadhu", "age":25},
 {"id":2, "name":"Ajay", "age":16},
 {"id":3, "name":"Syam", "age":22}]
 school[1]["Gender"]="Male"
-"""In this we can add Gender :male only for Ajay index[1]"""
+"""add Gender :male only for Ajay index[1]"""
 print(school)
 
 
