6b5e2139e019e070d67f3226e4b497de1ca784d2
Syamsaleel <Syamsaleel4066@gmail.com>
2023-02-22 11:01:03 +0530

    update

--- Syam saleel/day-03/dict.py
+++ Syam saleel/day-03/dict.py
@@ -1,5 +1,6 @@
 
 """VI. Consider below Dictionary"""
+
 current_dictionary = { 'product_id': '0x24e78',
 'product_label': 'strawberry',
 'fulfillment':
@@ -24,7 +25,23 @@ current_dictionary = { 'product_id': '0x24e78',
 ]}}
 
 
+def Mydict(key):
+    if key in current_dictionary.keys():
+        return current_dictionary[key]
+    else:
+        for data in current_dictionary:
+            if isinstance(current_dictionary[data],dict):
+                if key in current_dictionary[data].keys():
+                    return current_dictionary[data][key]
+            elif isinstance(current_dictionary[data], dict):
+                for item in current_dictionary[data]:
+                    if isinstance(item, dict) :
+                        if key in current_dictionary[item].keys():
+                            return current_dictionary [item][key]
+input_key = input("Enter a key to retrieve from the dictionary: ")
+print(Mydict(input_key))
 
+"""
 #2
 key =  input("enter the key:-")
 value = current_dictionary.get(key)
@@ -32,3 +49,4 @@ if value:
     print(value)
 else:
     print("Key is not at all present in the dictionary")
+    """
