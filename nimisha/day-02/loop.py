1ac9ba1f14a74db3f6ba228428664a2ba0874c73
Nimishanimi <nimishanimi123.nn@gmail.com>
2023-02-24 13:00:47 +0530

    updated

--- nimisha/day-02/loop.py
+++ nimisha/day-02/loop.py
@@ -3,29 +3,31 @@ Even Numbers : 10, 8, 6,4,2
 Odd Numbers : 9,7,6,5,3,1'''
 
 # using for loop
-# list=[10,9,8,7,6,5,4,3,2,1]
-# even=[]
-# odd=[]
-# for i in list :
-#   if(i % 2 ==0):
-#     even.append(i)
-#   else:
-#     odd.append(i)
-#     print(" even no:",even)
-#     print("odd no:",odd)
+evod=[10,9,8,7,6,5,4,3,2,1]
+even=[]
+odd=[]
+for i in evod :
+  if(i % 2 ==0):
+    even.append(i)
+  else:
+    odd.append(i) 
+print(" even no:",even)
+print(" odd no:",odd)
 
-    # using While loop
+    
 
 
-list1=[1,2,3,4,5,6,7,8,9,10]
+# using while loop
+
+evod1=[1,2,3,4,5,6,7,8,9,10]
 num=0
 even=[]
 odd=[]
-while(num<len(list1)):
-      if list1[num]% 2==0 :
-         even.append(list1[num])
+while(num<len(evod1)):
+      if evod1[num]% 2==0 :
+         even.append(evod1[num])
       else:
-         odd.append(list1[num])
+         odd.append(evod1[num])
         #  num += 1
-         print("even no:",even)
-         print("odd no:",odd)
+print("even no:",even)
+print("odd no:",odd)
