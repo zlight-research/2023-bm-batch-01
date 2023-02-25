5b1b28529f73ca12d2c3fae9a87f03de4db17a66
Syamsaleel <Syamsaleel4066@gmail.com>
2023-02-25 09:01:06 +0530

    changes

--- Syam saleel/day-03/dict.py
+++ Syam saleel/day-03/dict.py
@@ -33,11 +33,12 @@ def Mydict(key):
             if isinstance(current_dictionary[data],dict):
                 if key in current_dictionary[data].keys():
                     return current_dictionary[data][key]
-            elif isinstance(current_dictionary[data], dict):
-                for item in current_dictionary[data]:
-                    if isinstance(item, dict) :
-                        if key in current_dictionary[item].keys():
-                            return current_dictionary [item][key]
+                else: 
+                    if isinstance(current_dictionary[data], list):
+                     for item in current_dictionary[data]:
+                        if isinstance(item, dict) :
+                            if key in current_dictionary[item].keys():
+                                return current_dictionary [item][key]
 input_key = input("Enter a key to retrieve from the dictionary: ")
 print(Mydict(input_key))
 
