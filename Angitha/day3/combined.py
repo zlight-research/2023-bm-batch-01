b42820381fd09cd841b110778c9d34aa621c5310
Angithasheeban <angiacha535@gmail.com>
2023-02-27 10:32:30 +0530

    chnge

--- Angitha/day3/combined.py
+++ Angitha/day3/combined.py
@@ -8,36 +8,31 @@
 
 fields = ["wo_id", "work_order_date", "work_order_details"]
 data = [
+
     {"WO-01", "2022-05-06", "Identify what needs to be done to resolve a maintenance issue."},
     {"WO-02", "2022-05-07", "Complete a work order request form to authorize maintenance tasks."},
     {"WO-03", "2022-05-08", "Maintenance management decides if there is a legitimate need."}
 ]
-
-dict1=dict(zip(fields,data))
-
-
+dict1 = []
+# for item in data:
+    # dict1.append(dict(zip(fields,item)))
 # print(dict1)
-x=0
-
-while x<3:
-    x+=1
-    num=data[0],dict1=[]
-    dict1=dict(zip(fields,num))
-    print(dict1)
 
 
+dict2 = {}
+for item in data:
+    wo_id = dict(zip(fields, item))["wo_id"]
+    wo_date = dict(zip(fields, item))["work_order_date"]
+    wo_details = dict(zip(fields, item))["work_order_details"]
+    dict2[wo_id] = {
+        "work_order_details": wo_details,
+        "wo_date": wo_date
+    }
+print(dict2)
 
-# fields = ["wo_id", "work_order_date", "work_order_details"]
-# data = [
-#     {"WO-01", "2022-05-06", "Identify what needs to be done to resolve a maintenance issue."},
-#     {"WO-02", "2022-05-07", "Complete a work order request form to authorize maintenance tasks."},
-#     {"WO-03", "2022-05-08", "Maintenance management decides if there is a legitimate need."}
-# ]
-# # dict1=dict(zip(fields,data))
+
+    
 # dict1 = []
-# # print(data.index)
 # for item in data:
 #     dict1.append(dict(zip(fields,item)))
-    
 # print(dict1)
-
