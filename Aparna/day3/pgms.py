cbd8cdfec77a5aab61ba9d9cf3d3eeb189dea79d
aparnamathew95 <aparnaambattu@gmail.com>
2023-03-01 08:36:06 +0530

    pgm

--- Aparna/day3/pgms.py
+++ Aparna/day3/pgms.py
@@ -34,17 +34,17 @@ wo_data = [{"WO-01","2022-05-06","GENERAL","Identify what needs to be done to re
            {"WO-03","2022-05-08","EMERGENCY","Maintenance management decides if there is a legitimate need."},
            {"WO-04","2022-05-09","EMERGENCY","Work orders are assessed by considering the urgency, existing backlog, and team availability."}
           ]
-
+# creating new empty list
 new=[]
 for datas in wo_data:
     merge=zip(wo_fields,datas)
     con_dic=dict(merge)
     new.append(con_dic)
-
+# iterating data
 for data in new:
-    e={"emergency":[{"wo_id":data["wo_id"],"work_order_date":data["work_order_date"],"work_order_details":data["work_order_details"]}]}
-    g={"general":[{"wo_id":data["wo_id"],"work_order_date":data["work_order_date"],"work_order_details":data["work_order_details"]}]}
-print(e)
-print(g)
+    emergency={"emergency":[{"wo_id":data["wo_id"],"work_order_date":data["work_order_date"],"work_order_details":data["work_order_details"]}]}
+    general={"general":[{"wo_id":data["wo_id"],"work_order_date":data["work_order_date"],"work_order_details":data["work_order_details"]}]}
+print(emergency)
+print(general)
 # 2. Construct to 2 dict for above scenario. one for EMERGENCY and another for GENERAL
 
