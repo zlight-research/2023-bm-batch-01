be4c121519d7eb6d5b4a410e37cdccbf84230ed2
AthishaAthi <athishas123@gmail.com>
2023-03-03 14:06:57 +0530

    update

--- athisha/day-05/class.py
+++ athisha/day-05/class.py
@@ -29,37 +29,49 @@ seller_data = {
 """seller class initialize its attribute based on seller data"""
 # __init__ method initialize a seller object name, category,  items_in_stock.
 
+seller_data = {
+    "best store": {
+        "fresh fruits": [{"item": "apple", "price": 50}, {"item": "orange", "price": 80}, {"item": "banana", "price": 26}],
+        "vegetables": [{"item": "carrot", "price": 30}, {"item": "onion", "price": 65}, {"item": "zoya", "price": 15}]
+    },
+    "supreme": {
+        "cakes": [{"item": "black forest", "price": 450}, {"item": "white forest", "price": 520}, {"item": "red velvet", "price": 860}],
+        "drinks": [{"item": "pepsi", "price": 45}, {"item": "fanta", "price": 52}, {"item": "latte", "price": 100}]
+    }
+}
 
 class Item:
-    def __init__(self, item_name = "" , price = 0):
-        self.name = item_name
-        self.price = price 
-    
-    def set_item(self, item_name, price):
-        self.item_name = item_name
+    def __init__(self, name, price):
+        self.name = name
         self.price = price
 
 class Seller:
-    def __init__(self, seller_name = "",category = "", items_in_stock = {}):
-        self.seller_name = seller_name
-        self. category = category
-        self.items_in_stock = items_in_stock
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        self.items_in_stock = []
 
     def add_item(self, item_name, price):
-        item =Item(item_name, price)
-        if self.category in self.items_in_stock:
-            self.items_in_stock[self.self.category].append({"item": item.item_name, "price": item.price})
-        else:
-            self.items_in_stock[self.self.category]=[{"item": item.item_name, "price": item.price}]
-
-    def get_cost(self, item_name):
-        item_name = item_name.lower()
-        if self.category in self.items_in_stock:
-            for item in self.items_in_stock[self.category]:
-                if item["item"].lower() == item_name:
-                    return item
-        return "Item not found"
-
-    def is_item_present(self,item_name):
-        item_name = item_name.lower()
+        self.items_in_stock.append(Item(item_name, price))
         
+    def get_cost(self, item_name):
+        item = self.get_item(item_name)
+        return item.price if item else None
+
+    def get_item(self, item_name):
+        return next((item for item in self.items_in_stock if item.name.lower() == item_name.lower()), None)
+
+seller_name = input("Enter the name of the seller: ")
+category_name = input("Enter the name of the category: ")
+if (seller_data.get(seller_name) or {}).get(category_name):
+    item_name = input("Enter the name of the item: ")
+    item_price = float(input("Enter the price of the item: "))
+    seller = Seller(seller_name, category_name)
+    seller.add_item(item_name, item_price)
+    item = seller.get_item(item_name)
+    cost = item.price if item else None
+    has_item = bool(item)
+    print(f"{seller_name} - {category_name} - {item_name}: Cost={cost}, Has Item={has_item}")
+else:
+    print(f"The category '{category_name}' does not exist for the seller '{seller_name}'")
+
