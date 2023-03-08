cd24526c21e6406de04f13da7a04f5ae560173bc
Sreelekshmi321 <124665790+Sreelekshmi321@users.noreply.github.com>
2023-03-08 21:56:02 +0530

    Update pgm4.py

--- sreelekshmi s/day-03/pgm4.py
+++ sreelekshmi s/day-03/pgm4.py
@@ -1,8 +1,12 @@
 """III. Created two lists and combined both to create a dictionary as asked.
-new_list= ['WO-01', '2022-05-06', 'Identify what needs to be done to resolve a maintenance issue.',
-           '2022-05-07','Complete a work order request form to authorize maintenance tasks.', 'WO-02',
-           'Maintenance management decides if there is a legitimate need.', 'WO-03', '2022-05-08'
-          ]
+
+fields = ["wo_id","work_order_date","work_order_details"]
+
+data = [
+         {"WO-01","2022-05-06","Identify what needs to be done to resolve a maintenance issue."},
+         {"WO-02","2022-05-07","Complete a work order request form to authorize maintenance tasks."},
+         {"WO-03","2022-05-08","Maintenance management decides if there is a legitimate need."}
+       ]
 
 1. create below dict as given below.
 [{"wo_id": <WO-01>, "work_order_date":<>,"work_order_details":<>},
