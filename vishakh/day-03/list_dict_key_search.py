bbaf9813b403c5c4b123b4889182757c0bba27db
vishakhh <vishakhks22@gmail.com>
2023-03-13 10:04:09 +0530

    updated_day3_key_search

--- vishakh/day-03/list_dict_key_search.py
+++ vishakh/day-03/list_dict_key_search.py
@@ -1,34 +1,3 @@
-"""
-VI. Consider below Dictionary
-current_dictionary = { 'product_id': '0x24e78',
-'product_label': 'strawberry',
-'fulfillment':
-{ 'fulfillment_id': '0x24e73',
-'group_name': 'General',                    
-'services': [
-{
-'service_id': '0x24e70',
-'service_name': 'shipment',
-'service_division_criteria': 'business_id',
-'service_status': 'Packing Completion Pending',
-'service_function': 'Shipment system'
-},
-{
-'service_id': '0x24e72',
-'service_name': 'package',
-'service_division_criteria':
-'business_id',
-'service_status': 'Ready',
-'service_function': 'packaging system'
-}
-]}}
-
- 
-2. Traverse in the Dictionary to search for any Key, it should return the corresponding value without any errors.
-If Key is not present within the dictionary, it should display as "Key is not at all present in the dictionary"
-
-
-"""
 
 
 #create a dictionary
@@ -56,40 +25,55 @@ current_dictionary = { 'product_id': '0x24e78',
 }
 ]}}
 
+#create a empty list
+key_data=[]
 
-#enter a key in current dictionary
-        
-search_key=input("enter a key:").lower()
+#create a function key_find()
 
-
-#checking the key in current dictionary items and print the key value
+def key_find():
     
-for keys,values in current_dictionary.items():
-    if search_key==keys:
-       print(values)
+    #enter key for search
 
-    
-         
+    search_key=input("enter a key for search:")
+   
+    #checking the key in main dictionary find append to the empty list
+
+    for keys,values in current_dictionary.items():
+        if search_key==keys:
+           key_data.append(values)
+       
+
+       
+        #not in main dictionary then search key for values dictionaries find append to the empty list
         
-
-
-
-
-#checking the key in fulfillment dictionary items and print the key value
-
-for keys1,values1 in current_dictionary["fulfillment"].items():
-    if search_key==keys1:
-        print(values1)  
+        elif isinstance(values,dict):
+            for keys1,values1 in values.items():
+                if keys1==search_key:
+                   key_data.append(values1)
+                
+                #not search key  in values dictionry  then search values of list items find append to the empty list       
     
-               
-                  
-#checking the key in service item and print the key value
-        
-if keys1=="services":
-    for item in values1:
-        for keys2,values2 in item.items():
-            if search_key==keys2:
-                print(values2)
-            
+                elif isinstance(values1,list):
+                    for item in values1:
+                        for keys2,values2 in item.items():
+                           if keys2==search_key:
+                               key_data.append(values2)
 
-                    
+                          
+              
+    #the key is not in the list print key is not present inthe dictionary
+
+    if not key_data:
+
+        print("Key is not at all present in the dictionary")
+    
+    #the key is present in the list then print the values of search key
+    else:
+        for item in range(len(key_data)):
+            print(key_data[item])
+
+
+key_find()    
+
+
+
