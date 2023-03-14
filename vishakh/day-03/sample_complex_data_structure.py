75cf26a3126671e840d8e10c7fb1b6d4fff04cd1
vishakhh <vishakhks22@gmail.com>
2023-03-14 09:40:53 +0530

    day3-sample_complex_data_structure_updated

--- vishakh/day-03/sample_complex_data_structure.py
+++ vishakh/day-03/sample_complex_data_structure.py
@@ -15,13 +15,19 @@ II. Look at sample complex data structure
 
 
 '''
+#create a data employee_dic
 
 employee_dic=[{"id":1, "f_name":"yadhu", "l_name":"Kumar", "gender":"M", "age":25},
 {"id":2, "f_name":"Ajay", "l_name" : "Gopi", "gender":"M", "age":16},
 {"id":3, "f_name":"syam", "l_name":"Warrier", "gender":"F", "age":22}]
 
+#create two empty lists
+
 new_employee_dic1=[]
 new_employee_dic2=[]
+
+#iterate datas in list and adding a string to some values in dic
+
 for data in employee_dic:
 
   if data["gender"]=="M":
@@ -30,24 +36,31 @@ for data in employee_dic:
   else:
     data["f_name"]="Mrs."+data["f_name"]
     new_employee_dic2.append(data)
-employee_dic1=new_employee_dic1+new_employee_dic2
-print("*"*100)
-print(employee_dic1) 
-print("*"*100)
 
+#adding two lists
+
+employee_dic1=new_employee_dic1+new_employee_dic2
+
+#display first format ouput
+
+print(employee_dic1) 
+
+#two empty lists created
 age_less_or_eaqual_20=[]
 age_great_20=[]
+
+#iterate some values and conditiom check and append the values in to empty lists
+
 for data in employee_dic1:
     if data["age"]<=20:
 
-     age_less_or_eaqual_20.extend((data["f_name","l_name"]))
+      age_less_or_eaqual_20.append((data["f_name"],data["l_name"]))
 
     else:
      
-     age_great_20.extend((data["f_name","l_name"]))
+      age_great_20.append((data["f_name"],data["l_name"]))
 
-print(age_less_or_eaqual_20)
+#display the final result 
 
-print("*"*100)
-
-print(age_great_20)
+result={" Age <= 20":age_less_or_eaqual_20," Age > 20":age_great_20 }
+print(result)
