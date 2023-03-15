53fb24a90261d07ecbba2e0e6ed59c5404ca03a6
Syamsaleel <Syamsaleel4066@gmail.com>
2023-03-16 00:31:46 +0530

    update_dict_day_03

--- Syam saleel/day-03/dict.py
+++ Syam saleel/day-03/dict.py
@@ -23,12 +23,37 @@ current_dictionary = { 'product_id': '0x24e78',
 }
 ]}}
 
+def  searc_dict(key):
+    if key in current_dictionary.keys():
+        return current_dictionary[key]
+    else:
+        for data in current_dictionary:
+            if isinstance(current_dictionary[data],dict):
+                if key in current_dictionary[data].keys():
+                    return current_dictionary[data][key]
+            elif isinstance(current_dictionary[data], list):
+                for value in current_dictionary[data]:
+                    if isinstance (value, dict):
+                         if key in value.keys():
+                            if data == "services":
+                                return value[key]
+                            else:
+                                return "key not found"
+                        
+    return "key not found"
 
+
+print(searc_dict("service_status"))   
+"""               
+find_key = input("enter a key you want :")
+print(searc_dict(find_key))"""
 
 #2
+"""
 key = input("enter the key:-")
 value = current_dictionary.get(key)
 if value:
     print(value)
 else:
     print("Key is not at all present in the dictionary")
+"""
--- Syam saleel/day-03/tempCodeRunnerFile.py
+++ Syam saleel/day-03/tempCodeRunnerFile.py
@@ -1,13 +1,44 @@
-fields = ["wo_id", "work_order_date", "work_order_details"]
-data = [{"WO-01","2022-05-06","Identify what needs to be done to resolve a maintenance issue."},   
-             {"WO-02","2022-05-07","Complete a work order request form to authorize maintenance tasks."},   
-                       {"WO-03","2022-05-08","Maintenance management decides if there is a legitimate need."}]
-result = []
-for d in data:
-    wo_id = list(d)[0]
-    result.append({wo_id: {
-        "work_order_date": list(d)[1],
-        "work_order_details": list(d)[2]
-    }})
-print(result)
+current_dictionary = { 'product_id': '0x24e78',
+'product_label': 'strawberry',
+'fulfillment':
+{ 'fulfillment_id': '0x24e73',
+'group_name': 'General',                    
+'services': [
+{
+'service_id': '0x24e70',
+'service_name': 'shipment',
+'service_division_criteria': 'business_id',
+'service_status': 'Packing Completion Pending',
+'service_function': 'Shipment system'
+},
+{
+'service_id': '0x24e72',
+'service_name': 'package',
+'service_division_criteria':
+'business_id',
+'service_status': 'Ready',
+'service_function': 'packaging system'
+}
+]}}
 
+def  searc_dict(key):
+    if key in current_dictionary.keys():
+        return current_dictionary[key]
+    else:
+        for data in current_dictionary:
+            if isinstance(current_dictionary[data],dict):
+                if key in current_dictionary[data].keys():
+                    return current_dictionary[data][key]
+            elif isinstance(current_dictionary[data], list):
+                for value in current_dictionary[data]:
+                    if isinstance (value, dict):
+                         if key in value.keys():
+                            if data == "services":
+                                return value[key]
+                            else:
+                                return "key not found"
+                        
+    return "key not found"
+
+
+print(searc_dict("service_status")) 
