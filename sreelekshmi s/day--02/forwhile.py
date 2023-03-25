ddf888ef8e2c58cddc344474550fb5460706a8fa
Sreelekshmi321 <124665790+Sreelekshmi321@users.noreply.github.com>
2023-03-25 21:28:07 +0530

    Update forwhile.py

--- sreelekshmi s/day--02/forwhile.py
+++ sreelekshmi s/day--02/forwhile.py
@@ -1,17 +1,19 @@
 
-#Use FOR and WHILE loop to display the below manner.
-#Even Numbers : 10, 8, 6,4,2
-#Odd Numbers : 9,7,6,5,3,1
- 
-
 #WHILE LOOP
 
-print("odd  even")
+print("Even numbers:")
 i=1
 while i<10:
-    print(f"{i}\t{i+1}")
-    i+=2
-
+    if  i%2==0:
+      print(i,',',end=" " )
+    i+=1
+print('\n')
+print("Odd numbers:")
+i=1
+while i<10:
+    if  i%2==1:
+      print(i,',',end=" " )
+    i+=1    
 
 
 
@@ -19,11 +21,29 @@ while i<10:
 
 i=1
 end=10
+print('Even numbers:',end=' ' )
 for i in range(i, end):
     if  i%2==0:
-      print("even number:",i,end=" " )
-    else:
-      print("Odd number:",i,end=" " )
+     print(i,',',end=" " )
+print('\n')  
+
+i=1
+end=10
+print('Odd numbers:',end=' ' )
+for i in range(i, end): 
+    if  i%2==1:
+      print(i,',',end=" " )
+
+
+      
+
+
+
+
+
+
+
+
 
 
       
